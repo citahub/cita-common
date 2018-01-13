f6e95bbec35e82bf4519ee129afa2fa61570317a
Boyu Yang <yangby@cryptape.com>
2018-01-13 11:58:20 +0800

    Fix the script which generates codes from proto files.

--- libproto/src/create_protobuf.sh
+++ libproto/src/create_protobuf.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-protoc blockchain.proto --rust_out .
-protoc communication.proto --rust_out .
-protoc auth.proto --rust_out .
-protoc request.proto --rust_out .
-protoc response.proto --rust_out .
-protoc consensus.proto --rust_out .
-protoc sync.proto --rust_out .
-protoc executer.proto --rust_out .
+
+find . -name "*.proto" | while read protofile; do
+    protoc ${protofile} --rust_out .
+done
 
 case "$OSTYPE" in
-    darwin*)  
+    darwin*)
         sed -ig 's/    data: ::std::option::Option<Response_oneof_data>,/    pub data: ::std::option::Option<Response_oneof_data>,/g' response.rs
         sed -ig 's/    req: ::std::option::Option<Request_oneof_req>,/    pub req: ::std::option::Option<Request_oneof_req>,/g' request.rs
-        ;; 
-    *)       
+        ;;
+    *)
         sed -i 's/    data: ::std::option::Option<Response_oneof_data>,/    pub data: ::std::option::Option<Response_oneof_data>,/g' response.rs
         sed -i 's/    req: ::std::option::Option<Request_oneof_req>,/    pub req: ::std::option::Option<Request_oneof_req>,/g' request.rs
         ;;
@@ -21,11 +17,11 @@ esac
 
 for i in `find . -name "*.rs"`
 do
-    if grep -q -e "Copyright 2015-2017 Parity Technologies" -e "Copyright 2016-2017 Cryptape Technologies" $i
+    if grep -q -e "Copyright 2015-20.. Parity Technologies" -e "Copyright 2016-20.. Cryptape Technologies" $i
     then
         echo "Ignoring the " $i
     else
         echo "Starting modify" $i
-        (cat ../../../publishtool/notices | cat - $i > file1) && mv file1 $i
+        (cat ../../LICENSE_HEADER | cat - $i > file1) && mv file1 $i
     fi
 done
