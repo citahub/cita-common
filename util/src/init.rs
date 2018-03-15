2766a0399560fabe774b3d5fe0d1c5e64d2e077f
wangzhanfeng310103 <wangzhanfeng@cryptape.com>
2018-03-15 11:13:07 +0800

    add macro for parsing config

--- util/src/init.rs
+++ util/src/init.rs
@@ -1,3 +1,6 @@
+use serde::de;
+use toml;
+
 #[macro_export]
 macro_rules! micro_service_init {
     ($x:expr, $y:expr) => {
@@ -12,4 +15,26 @@ macro_rules! micro_service_init {
         logger::init_config($x);
         info!($y);
     };
+}
+
+pub fn parse_config_from_buffer<'de, T>(s: &'de str) -> Result<T, toml::de::Error>
+where
+    T: de::Deserialize<'de>,
+{
+    Ok(toml::from_str::<T>(s)?)
+}
+
+#[macro_export]
+macro_rules! parse_config {
+    ($type:ty,$path:expr) => {{
+        use std::fs::File;
+        use std::io::Read;
+        use util::init::parse_config_from_buffer;
+
+        let mut buffer = String::new();
+        File::open($path).and_then(|mut f| {
+        f.read_to_string(&mut buffer)
+        }).unwrap_or_else(|err| panic!("Error while loading config: [{}]", err));
+        parse_config_from_buffer::<$type>(&buffer).unwrap_or_else(|err| panic!("Error while parsing config: [{}]", err))
+    }};
 }
