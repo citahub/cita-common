01cd1a08a9f28a9232f5130fcd46af976dafbee0
rink1969 <rink1969@cryptape.com>
2018-04-24 11:59:48 +0800

    fix licenses

--- util/src/avl/fatdb.rs
+++ util/src/avl/fatdb.rs
@@ -1,21 +1,21 @@
 // Copyright 2015-2017 Parity Technologies (UK) Ltd.
 // This file is part of Parity.
 
-// Parity is free software: you can redistribute it and/or modify
+// Copyright 2016-2017 Cryptape Technologies LLC.
+// Replace trie tree with avl tree
+
+// This software is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// Parity is distributed in the hope that it will be useful,
+// This software is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
-
-// CITA, Copyright 2016-2017 Cryptape Technologies LLC.
-// Replace trie tree with avl tree
 
 use super::{AVLDBIterator, AVLItem, AVLIterator, Query, AVL, AVLDB};
 use H256;
