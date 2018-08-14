48948968b913f8173d4005e2b04de64ac926d882
Boyu Yang <yangby@cryptape.com>
2018-08-14 09:51:13 +0800

    Add benches for hash and sign algorithms.

---
+++ cita-common-benches/src/lib.rs
@@ -0,0 +1,10 @@
+extern crate rand;
+
+pub mod bench_tools {
+
+    use rand::{thread_rng, Rng};
+
+    pub fn random_bytes(len: usize) -> Vec<u8> {
+        thread_rng().gen_iter::<u8>().take(len).collect::<Vec<u8>>()
+    }
+}
