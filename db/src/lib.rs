950eeb1c6afd9b08f35bfb9050919713454b31f8
zeroqn <zeroqn@users.noreply.github.com>
2018-12-27 02:56:30 +0000

    Split db from util (#178)
    * chore: db: extract varied db code from `util` crate into `db` crate
    
    `util` crate contains a lot of useful helper functions. Unfortunately,
    it brings in rocksdb, which cause a lot of time to compile. It's
    better that we move db code into its own crate.
    
    * chore: db: fix compilation
    
    * chore: db: fix tests
    
    * chore: util: remove db code
    
    * chore: util: remove unused dev-dependency
    
    * chore: util: remove unused `hashable` dep and features
    
    * doc: README: add db crate information
    
    * chore: ci: add db and remove util from hash feature tests

---
+++ db/src/lib.rs
@@ -0,0 +1,31 @@
+#![feature(custom_attribute)]
+#![allow(unused_attributes)]
+
+extern crate cita_types as types;
+extern crate elastic_array;
+#[macro_use]
+extern crate logger;
+extern crate hashable;
+extern crate heapsize;
+extern crate itertools;
+extern crate parity_rocksdb;
+extern crate regex;
+extern crate rlp;
+extern crate util;
+
+pub mod avl;
+pub mod hashdb;
+pub mod journaldb;
+pub mod kvdb;
+pub mod memorydb;
+pub mod overlaydb;
+pub mod trie;
+pub mod triehash;
+
+pub use hashdb::*;
+pub use journaldb::JournalDB;
+pub use kvdb::*;
+pub use memorydb::MemoryDB;
+pub use overlaydb::*;
+pub use trie::{SecTrieDB, SecTrieDBMut, Trie, TrieDB, TrieDBMut, TrieError, TrieFactory, TrieMut};
+pub use triehash::*;
