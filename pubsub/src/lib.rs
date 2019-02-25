9c24c330b8b5e9556c56d876ab32edb38751cf08
kaikaifeng <kaikai@cryptape.com>
2019-02-25 17:29:39 +0800

    export crate crossbram_channel.

--- pubsub/src/lib.rs
+++ pubsub/src/lib.rs
@@ -14,7 +14,7 @@
 
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-extern crate crossbeam_channel as channel;
+pub extern crate crossbeam_channel as channel;
 extern crate dotenv;
 #[cfg(feature = "kafka")]
 extern crate pubsub_kafka;
