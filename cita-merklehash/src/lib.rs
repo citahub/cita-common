f9df874403556452bb0433af55b1568dc747e1dd
kaikai1024 <kaikaifeng1024@gmail.com>
2019-06-12 14:09:07 +0800

    Update cita-merklehash to 2018 edition.

--- cita-merklehash/src/lib.rs
+++ cita-merklehash/src/lib.rs
@@ -1,5 +1,5 @@
 // CITA
-// Copyright 2016-2018 Cryptape Technologies LLC.
+// Copyright 2016-2019 Cryptape Technologies LLC.
 
 // This program is free software: you can redistribute it
 // and/or modify it under the terms of the GNU General Public
@@ -15,12 +15,9 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-extern crate cita_types;
 extern crate hashable;
-extern crate rlp;
 #[macro_use]
 extern crate rlp_derive;
-extern crate static_merkle_tree;
 
 use self::hashable::Hashable;
 use cita_types::H256;
