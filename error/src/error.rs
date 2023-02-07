0fdbf66261aee03baabceaf3dd3f6add2aab328c
ypf <yuitta@163.com>
2023-02-07 11:29:26 +0800

    optim: update light node error

--- error/src/error.rs
+++ error/src/error.rs
@@ -15,10 +15,48 @@
 //-32003             查询类错误
 //-32006             交易认证类错误
 //-32099             请求超时
+// L at last mean light node
+//-32501             轻节点-无可用节点*
+//-32502             轻节点-区块头未同步*
+//-32503             轻节点-回执检查失败
+//-32504             轻节点-未找到回执
+//-32505             轻节点-未找到交易
+//-32506             轻节点-未找到区块
+//-32507             轻节点-未找到区块头
+//-32508             轻节点-交易检查失败
+//-32509             轻节点-状态数据查询失败
 pub enum ErrorCode {
     QueryError,
     TxAuthError,
     TimeOut,
+    NoneLinkableNodeL,
+    NotSyncedHeaderL,
+    ReceiptCheckErrorL,
+    NotFoundReceiptL,
+    NotFoundTransactionL,
+    NotFoundBlockL,
+    NotFoundBlockHeaderL,
+    TransactionCheckErrorL,
+    QueryErrorL,
+}
+
+impl ErrorCode {
+    pub fn to_error_code(&self) -> i64 {
+        match self {
+            ErrorCode::QueryError => ErrorCode::query_error(),
+            ErrorCode::TxAuthError => ErrorCode::tx_auth_error(),
+            ErrorCode::TimeOut => ErrorCode::time_out_error(),
+            ErrorCode::NoneLinkableNodeL => ErrorCode::no_linkable_node_l(),
+            ErrorCode::NotSyncedHeaderL => ErrorCode::not_synced_header_l(),
+            ErrorCode::ReceiptCheckErrorL => ErrorCode::receipt_check_error_l(),
+            ErrorCode::NotFoundReceiptL => ErrorCode::not_found_receipt_l(),
+            ErrorCode::NotFoundTransactionL => ErrorCode::not_found_transaction_l(),
+            ErrorCode::NotFoundBlockL => ErrorCode::not_found_block_l(),
+            ErrorCode::NotFoundBlockHeaderL => ErrorCode::not_found_block_header_l(),
+            ErrorCode::TransactionCheckErrorL => ErrorCode::transaction_check_error_l(),
+            ErrorCode::QueryErrorL => ErrorCode::query_error_l(),
+        }
+    }
 }
 
 impl ErrorCode {
@@ -32,5 +70,41 @@ impl ErrorCode {
 
     pub fn time_out_error() -> i64 {
         -32099
+    }
+
+    pub fn no_linkable_node_l() -> i64 {
+        -32501
+    }
+
+    pub fn not_synced_header_l() -> i64 {
+        -32502
+    }
+
+    pub fn receipt_check_error_l() -> i64 {
+        -32503
+    }
+
+    pub fn not_found_receipt_l() -> i64 {
+        -32504
+    }
+
+    pub fn not_found_transaction_l() -> i64 {
+        -32505
+    }
+
+    pub fn not_found_block_l() -> i64 {
+        -32506
+    }
+
+    pub fn not_found_block_header_l() -> i64 {
+        -32507
+    }
+
+    pub fn transaction_check_error_l() -> i64 {
+        -32508
+    }
+
+    pub fn query_error_l() -> i64 {
+        -32509
     }
 }
