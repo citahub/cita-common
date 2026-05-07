94561725824c1acfa4df2a90f34827a458b16c72
yieazy <yuitta@163.com>
2026-05-07 14:12:08 +0800

    optim: set_var add unsafe

--- util/src/init.rs
+++ util/src/init.rs
@@ -20,8 +20,10 @@ use toml;
 macro_rules! micro_service_init {
     ($x:expr, $y:expr, $s:expr) => {
         dotenv::dotenv().ok();
-        // Always print backtrace on panic.
-        ::std::env::set_var("RUST_BACKTRACE", "full");
+        unsafe {
+            // Always print backtrace on panic.
+            ::std::env::set_var("RUST_BACKTRACE", "full");
+        }
 
         //exit process when panic
         set_panic_handler();
